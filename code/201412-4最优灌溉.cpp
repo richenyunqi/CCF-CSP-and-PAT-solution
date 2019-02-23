0d961e01a738cfbf6d5150c8beb15ec9ad7d3ba4
日沉云起 <csjiangfeng1212@163.com>
2019-02-23 22:26:35 +0800

    Update 201412-4最优灌溉.cpp

--- code/201412-4最优灌溉.cpp
+++ code/201412-4最优灌溉.cpp
@@ -1,44 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 struct Edge{//边的类
-    int v1,v2,cost;//边的两个端点，以及边的花费
-    Edge(int vv1,int vv2,int c):v1(vv1),v2(vv2),cost(c) {}//构造函数
+    int v1,v2,cost;
+    Edge(int vv1,int vv2,int c):v1(vv1),v2(vv2),cost(c){}
     bool operator <(const Edge&e)const{//重载小于运算符
         return this->cost>e.cost;
     }
 };
-int N,M,father[1005];
-priority_queue<Edge>pq;//边的优先级队列
-int findFather(int x){//查找并查集中某一顶点的根节点
-    if(x==father[x])
+priority_queue<Edge>edges;
+int father[100005];//并查集
+int findFather(int x){//查找根结点并进行路径压缩
+    if(father[x]==x)
         return x;
     int temp=findFather(father[x]);
     father[x]=temp;
     return temp;
 }
-void unionSet(int a,int b){//合并并查集的两个顶点
-    int ua=findFather(a),ub=findFather(b);
-    if(ua!=ub)
-        father[ua]=ub;
-}
 int main(){
-    scanf("%d%d",&N,&M);
-    for(int i=0;i<M;++i){
+    int n,m,ans=0;
+    scanf("%d%d",&n,&m);
+    iota(father,father+n+1,0);//初始化并查集
+    while(m--){
         int a,b,c;
         scanf("%d%d%d",&a,&b,&c);
-        pq.push(Edge(a,b,c));
+        edges.push(Edge(a,b,c));
     }
-    for(int i=0;i<N+1;++i)
-        father[i]=i;
-    int sumCost=0;
-    while(!pq.empty()){
-        Edge e=pq.top();//弹出一条边
-        pq.pop();
-        if(findFather(e.v1)!=findFather(e.v2)){
-            unionSet(e.v1,e.v2);
-            sumCost+=e.cost;
+    while(!edges.empty()){
+        Edge e=edges.top();
+        edges.pop();
+        int ua=findFather(e.v1),ub=findFather(e.v2);
+        if(ua!=ub){//边的两个端点不属于同一个集合
+            father[ua]=ub;
+            ans+=e.cost;
         }
     }
-    printf("%d",sumCost);
+    printf("%d",ans);
     return 0;
 }
