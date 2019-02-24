908097987184f47939c1a065f7028c5ebc6027db
日沉云起 <csjiangfeng1212@163.com>
2019-02-24 13:28:13 +0800

    Update 201409-4最优配餐.cpp

--- code/201409-4最优配餐.cpp
+++ code/201409-4最优配餐.cpp
@@ -1,47 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
-int N,M,K,D;
-bool visit[1005][1005];
+int n,m,k,d,direction[4][2]={{-1,0},{0,1},{1,0},{0,-1}};//由某点可到达的四个位置
 int graph[1005][1005];//相应元素值<0、==0、>0分别表示不能通过位置、能通过位置、客户位置
-struct Tuple{
-    int i,j,cost;//横坐标、纵坐标，到达该位置时的最短路径长
-    Tuple(int ii,int jj,int c):i(ii),j(jj),cost(c) {}
+bool inQueue[1005][1005];
+struct Position{
+    int x,y,cost;//横坐标、纵坐标，到达该位置时的最短路径长
+    Position(int xx,int yy,int c=0):x(xx),y(yy),cost(c) {}
 };
-bool outOfBorder(int i,int j){//是否越界
-    return i<1||j<1||i>N||j>N||graph[i][j]<0;
-}
 int main(){
-    scanf("%d%d%d%d",&N,&M,&K,&D);
-    queue<Tuple>q;
+    scanf("%d%d%d%d",&n,&m,&k,&d);
+    queue<Position>q;
     int a,b,c;
-    for(int i=0;i<M;++i){
+    long long ans=0;//最终结果需用long long储存
+    while(m--){
         scanf("%d%d",&a,&b);
-        q.push(Tuple(a,b,0));//将源点都压入队列中
+        q.push(Position(a,b));//将源点都压入队列中
+        inQueue[a][b]=true;//源点都已入队
     }
-    for(int i=0;i<K;++i){
+    while(k--){
         scanf("%d%d%d",&a,&b,&c);
-        graph[a][b]+=c;
+        graph[a][b]+=c;//同一个位置可以有多个客户，使用+=，而不是直接赋值
     }
-    for(int i=0;i<D;++i){
+    while(d--){
         scanf("%d%d",&a,&b);
         graph[a][b]=-1;//不能通过的位置置-1
     }
-    int direction[4][2]={
-        {-1,0},{0,1},{1,0},{0,-1}//由某点可到达的四个位置，上右下左顺序
-    };
-    long long sumCost=0;//最终结果需用long long储存
-    while(!q.empty()){//开始BFS遍历
-        Tuple p=q.front();
+    while(!q.empty()){
+        Position p=q.front();
         q.pop();
-        if(visit[p.i][p.j])//该点已访问过，不进行操作，直接继续循环
-            continue;
-        visit[p.i][p.j]=true;
-        if(graph[p.i][p.j]>0)//该位置是客户位置
-            sumCost+=(long long)p.cost*graph[p.i][p.j];//累加花费
-        for(int j=0;j<4;++j)//遍历该位置能到达的位置
-            if(!outOfBorder(p.i+direction[j][0],p.j+direction[j][1])&&!visit[p.i+direction[j][0]][p.j+direction[j][1]])
-                q.push(Tuple(p.i+direction[j][0],p.j+direction[j][1],p.cost+1));
+        if(graph[p.x][p.y]>0)//该位置是客户位置
+            ans+=(long long)p.cost*graph[p.x][p.y];//累加花费
+        for(int i=0;i<4;++i){//遍历该位置能到达的位置
+            int ii=p.x+direction[i][0],jj=p.y+direction[i][1];//下一个可到达的位置
+            if(ii>0&&ii<=n&&jj>0&&jj<=n&&graph[ii][jj]>=0&&!inQueue[ii][jj]){
+                q.push(Position(ii,jj,p.cost+1));
+                inQueue[ii][jj]=true;
+            }
+        }
     }
-    printf("%lld",sumCost);
+    printf("%lld",ans);
     return 0;
 }
