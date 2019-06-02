434134a4774117f3fbddf1376fb2af54992a8577
日沉云起 <csjiangfeng1212@163.com>
2019-06-02 14:28:42 +0800

    Update 201403-2窗口.cpp

--- code/201403-2窗口.cpp
+++ code/201403-2窗口.cpp
@@ -1,22 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
-//窗口类 
-struct Window{
+struct Window{//窗口类 
 	int num;//编号
 	int x1,y1,x2,y2; //左上角和右下角坐标 
 };
-//判断点击的坐标(a,b)是否在窗口w中 
-bool InWindow(Window&w,int a,int b){
-	if(a>=w.x1&&a<=w.x2&&b>=w.y1&&b<=w.y2)
-		return true;
-	return false;
-}
 int main(){
 	int N,M;
 	scanf("%d%d",&N,&M);
 	list<Window>window;//窗口的list 
-	//读取数据并加入list中 
-	for(int i=1;i<=N;++i){
+	for(int i=1;i<=N;++i){//读取数据并加入list中 
 		Window w;
 		scanf("%d%d%d%d",&w.x1,&w.y1,&w.x2,&w.y2);
 		w.num=i;
@@ -25,9 +17,8 @@ int main(){
 	while(M--){
 		int a,b;
 		scanf("%d%d",&a,&b);
-		list<Window>::iterator i=window.begin();
-		//查找list中有没有点击包含点击坐标的窗口 
-		while(i!=window.end()&&!InWindow(*i,a,b)) 
+		auto i=window.begin();
+		while(i!=window.end()&&!(a>=i->x1&&a<=i->x2&&b>=i->y1&&b<=i->y2))//查找list中有没有点击包含点击坐标的窗口 
 			++i;
 		if(i==window.end())//没有输出IGNORED 
 			printf("IGNORED\n");
