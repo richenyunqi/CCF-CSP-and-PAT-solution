b8535434c29f871a953958a9ca0f91e5c97add61
日沉云起 <csjiangfeng1212@163.com>
2019-06-02 14:47:56 +0800

    Update 201503-2数字排序.cpp

--- code/201503-2数字排序.cpp
+++ code/201503-2数字排序.cpp
@@ -1,33 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-struct T{
-	int data;//数字 
-	int num;//出现次数 
-	T(int d,int n):data(d),num(n){}//构造函数 
-};
-//比较函数 
-bool cmp(const T&t1,const T&t2){
-	if(t1.num!=t2.num)
-		return t1.num>t2.num;
-	else 
-		return t1.data<t2.data;
-}
 int main(){
-	int N;
-	scanf("%d",&N);
-	map<int,int>m;
-	for(int i=0;i<N;++i){
-		int a;
-		scanf("%d",&a);
-		++m[a];//将读取的数字出现次数递增 
-	}
-	vector<T>v;
-	//将map中元素搬迁到vector中 
-	for(map<int,int>::iterator i=m.begin();i!=m.end();++i)
-		v.push_back(T(i->first,i->second));
-	sort(v.begin(),v.end(),cmp);//排序
-	//输出 
-	for(int i=0;i<v.size();++i)
-		printf("%d %d\n",v[i].data,v[i].num);
-	return 0;
-} 
+    int N,a;
+    scanf("%d",&N);
+    unordered_map<int,int>m;
+    for(int i=0;i<N;++i){
+        scanf("%d",&a);
+        ++m[a];//将读取的数字出现次数递增
+    }
+    vector<pair<int,int>>v;
+    for(auto i:m)//将map中元素搬迁到vector中,注意first表示数字,second表示数字出现的次数
+        v.push_back(i);
+    sort(v.begin(),v.end(),[](const pair<int,int>&p1,const pair<int,int>&p2){
+        return p1.second!=p2.second?p1.second>p2.second:p1.first<p2.first;
+    });//排序
+    //输出
+    for(auto i:v)
+        printf("%d %d\n",i.first,i.second);
+    return 0;
+}
