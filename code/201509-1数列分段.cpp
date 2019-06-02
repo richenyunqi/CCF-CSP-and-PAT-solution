ac11ec82c2ee1761918ae332357eb82033b1792c
日沉云起 <csjiangfeng1212@163.com>
2019-06-02 13:56:12 +0800

    Update 201509-1数列分段.cpp

--- code/201509-1数列分段.cpp
+++ code/201509-1数列分段.cpp
@@ -1,10 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	int N;
-	scanf("%d",&N);
-	int num=1,current; 
-	scanf("%d",¤t);
+	int N,num=1,current;
+	scanf("%d%d",&N,&current);
 	for(int i=1;i<N;++i){
 		int a;
 		scanf("%d",&a);
